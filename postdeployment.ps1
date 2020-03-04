ab648e82306e28853c7983f14cd9afe5d7ea720c
shimulhumayun <h.shimul921@gmail.com>
2020-03-04 13:21:10 -0500

    Update postdeployment.ps1

--- postdeployment.ps1
+++ postdeployment.ps1
@@ -1,3 +1,3 @@
 $password=ConvertTo-SecureString "changePassword2020" -AsPlainText -Force
 New-LocalUser -Name "tesuser" -Description "Test user account." -Password $password
-Add-LocalGroupMember -Group "Administrators" -Member "shimul"
+Add-LocalGroupMember -Group "Administrators" -Member "tesuser"
